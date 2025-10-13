3ed088934fb213a74549e03846eaf32f3c66b267
pfingold <pfingold@uchicago.edu>
2025-10-13 11:07:26 -0500

    updated files

--- Assignment 1/calc_execution_metrics.py
+++ Assignment 1/calc_execution_metrics.py
@@ -18,50 +18,97 @@ import pandas as pd
 
 OUTPUT_COLUMNS = ["LastMkt", "AvgPriceImprovement", "AvgExecSpeedSecs"]
 
-#Parse command line arguments:
-parser = argparse.ArgumentParser(description = "Convert FIX to CSV")
+def parse_inputs():
+    """
+    Helper function to parse command line inputs
+    """
+    parser = argparse.ArgumentParser(description = "Convert FIX to CSV")
 
-parser.add_argument("--input_csv_file", type = str, help = "Path to CSV Input")
-parser.add_argument("--output_metrics_file", type = str, help = "Path to Metrics Output")
+    parser.add_argument("--input_csv_file", type = str, help = "Path to CSV Input")
+    parser.add_argument("--output_metrics_file", type = str, help = "Path to Metrics Output")
 
-args = parser.parse_args()
+    print("Sucess - parsed inputs!")
+    return parser.parse_args()
 
-input_csv_file = args.input_csv_file
-output_metrics_file = args.output_metrics_file
+def calculate_AvgPriceImprovement(exchange):
+    """
+    Takes an exchange (GroupBy item) and calculates 
+    the average savings by comparing limit price vs 
+    average execution price
+    """
+    exchange_size = len(exchange)
+    #Convert data to numeric values:
+    limit_price = pd.to_numeric(exchange["LimitPrice"], errors = "coerce")
+    average_price = pd.to_numeric(exchange["AvgPx"], errors = "coerce")
 
-#Read csv file as pandas:
-input_df = pd.read_csv(input_csv_file)
+    #Calculate average savings
+    avg_price_improvement = (limit_price - average_price).mean()
+    return avg_price_improvement
+    
 
-output_contents = pd.DataFrame(index = OUTPUT_COLUMNS)
+def calculate_AvgExecSpeedSecs(exchange):
+    """
+    Takes an exchange (GroupBy item) and calculates 
+    the average execution speed in seconds
+    """
+    exchange_size = len(exchange)
+    #Convert data to datetime values:
+    execution_time = pd.to_datetime(exchange["ExecutionTransactTime"], errors = "coerce")
+    transaction_time =  pd.to_datetime(exchange["OrderTransactTime"], errors = "coerce")
 
-#Split by Exchange ID:
-exchanges = input_df.groupby(["LastMkt"])
+    avg_execution_time = (execution_time - transaction_time).mean()
+    #Convert to seconds
+    avg_execution_time_sec = avg_execution_time.total_seconds()
+    return avg_execution_time_sec
 
-#Calculate metrics for each Exchange:
-for exchange in exchanges:
-    exchange_size = exchange.count()
+def clean_exchange_name(exchange):
+    """
+    Helper function that removes symbols from exchange name
+    """
+    name = str(exchange)
+    extra_chars = ["," , "(", ")", "'"]
+    for char in extra_chars:
+        name = name.replace(char, "")
 
-    #Average Execution Speed
-    total_execution_time = exchange.apply(lambda x: x["ExecutionTransactTime"] -
-                                          x["OrderTransactTime"]).sum()
-    avg_execution_time = total_execution_time / exchange_size
+    clean_name = name.strip()
+    return clean_name
 
-    #Average Savings
-    total_execution_price = exchange.apply(lambda x: x["LimitPrice"] -
-                                           x["AvgPx"]).sum()
-    #^does this take the sum of all the orders per exchange?
-    avg_price_improvement = total_execution_price / exchange_size
-
-    new_output_row = [exchange, avg_price_improvement, avg_execution_time]
-
-    #add new row to output dataframe
-    output_contents = pd.concat([output_contents, pd.DataFrame(new_output_row)],
-                                ignore_index = True)
-
-output_metrics_file = output_contents.to_csv("output_metrics_file", columns = OUTPUT_COLUMNS)
 
 def main():
-    ...
+    args = parse_inputs()
 
-if __name__ == "main":
+    input_csv_file = args.input_csv_file
+    output_metrics_file = args.output_metrics_file
+
+    #Read csv file as pandas:
+    input_df = pd.read_csv(input_csv_file)
+
+    output_contents = pd.DataFrame(columns = OUTPUT_COLUMNS)
+    #output_contents = output_contents.reindex(columns = OUTPUT_COLUMNS)
+
+    #Split by Exchange ID:
+    exchanges = input_df.groupby(["LastMkt"])
+
+    #Calculate metrics for each Exchange:
+    for exchange_name, exchange_data in exchanges:
+        cleaned_name = clean_exchange_name(exchange_name)
+        AvgPriceImprovement = calculate_AvgPriceImprovement(exchange_data)
+        AvgExecSpeedSecs = calculate_AvgExecSpeedSecs(exchange_data)
+
+        new_output_row = {"LastMkt" : cleaned_name, 
+                          "AvgPriceImprovement" : AvgPriceImprovement, 
+                          "AvgExecSpeedSecs" : AvgExecSpeedSecs
+                          }
+        print("Added a new row of metrics!")
+
+        #add new row to output dataframe
+        output_contents = pd.concat([output_contents, pd.DataFrame([new_output_row])],
+                                    ignore_index = True)
+
+    output_metrics_file = output_contents.to_csv(args.output_metrics_file, index = False)
+    print("Success - Calculated Metrics & Converted to CSV")
+    return output_metrics_file
+
+
+if __name__ == "__main__":
     main()
